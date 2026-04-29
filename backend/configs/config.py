71aa1a6683c88ea0dacf663c76da6c5979e9d4db
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2026-04-29 16:58:01 +0500

    login service complete

--- backend/configs/config.py
+++ backend/configs/config.py
@@ -4,6 +4,7 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 admin_email = os.getenv('admin_email')
 admin_password = os.getenv('admin_password')
+secret_key = os.getenv('secret_key')
 URL=None
 if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
     URL= DATABASE_URL.replace("postgresql://","postgresql+psycopg://")
--- backend/lib/token_lib.py
+++ backend/lib/token_lib.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta,UTC
 from typing import Optional
 from jose import jwt,JWTError
-
+from configs.config import secret_key
 def create_token(data:dict,expires_delta:Optional[timedelta] = None):
     """create a signed jwt token"""
     to_encode = data.copy()
@@ -9,18 +9,18 @@ def create_token(data:dict,expires_delta:Optional[timedelta] = None):
     exp =  datetime.now(UTC) + (expires_delta or timedelta(minutes=30))
     to_encode.update({"exp":exp})
 
-    return jwt.encode(to_encode,"aadsdsdsdsdsdsdsfdfeffwdwrew",algorithm="HS256")
+    return jwt.encode(to_encode,secret_key,algorithm="HS256")
 
 
 def decode_token(token:str):
     """Decode adn validate the JWT token"""
     try:
-        return jwt.decode(token=token,key="aadsdsdsdsdsdsdsfdfeffwdwrew",algorithms=["HS256"])
+        return jwt.decode(token=token,key=secret_key,algorithms=["HS256"])
     except JWTError:
         return None    
 
-# # token = create_token(data={"name":"Muhammad Talha","email":"muhammadtalha0110@gmail.com","role":"admin"})
+# # token = create_token(data={})
 
 # # print(token)
-# token = decode_token("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4iLCJleHAiOjE3Nzc0NTQ5ODh9.Tt2G5bL4bmQZFv2s2LuK2gEX60vaOizx95Jd1cu0lXo")
+# token = decode_token("")
 # print(token)
